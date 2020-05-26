a8e3b51e71d9d7a59000a74623ee956232994f35
Sebastian <acatonmars@outlook.com>
2020-05-25 20:47:18 -0500

    State pattern - traffic light

--- State pattern/main.js
+++ State pattern/main.js
@@ -1,18 +1,47 @@
+const lights = document.querySelectorAll(".light")
+
 class TrafficLight {
     constructor() {
         this.count = 0;
+        this.light = 0;
         this.currentState = new Red(this);
     }
 
     change(state) {
-        if (count++ >= 10) return;
+        if (++this.count >= 10) return;
         this.currentState = state;
-        currentState.switch();
+
+        for (let light of lights) {
+            light.classList.remove("glowing")
+        }
+
+        let color = getComputedStyle(lights[this.light]).getPropertyValue("background").split(")")[0] + ")"
+        console.log(`color: ${color}`)
+        lights[this.light].classList.toggle("glowing")
+
+        // console.log(lights[this.light].style.background)
+        document.documentElement.style
+            .setProperty('--glowing-color', color);
+
+
+
+        console.log(this.light)
+        this.light++
+        if (this.light > 2) {
+            this.light = 0
+        }
+
+        state.switch(state)
+
     }
 
     start() {
-        currentState.switch();
+        this.currentState.switch(this.currentState);
     }
+}
+
+function wow({ light }) {
+    return light
 }
 
 class Red {
@@ -20,9 +49,11 @@ class Red {
         this.light = light
     }
 
-    switch() {
-        // log.add()
-        light.change(new Green(light))
+    switch({ light }) {
+
+        setTimeout(() => {
+            light.change(new Green(light))
+        }, 2000)
     }
 }
 
@@ -31,8 +62,11 @@ class Green {
         this.light = light
     }
 
-    switch() {
-        light.change(new Yellow(light))
+    switch({ light }) {
+
+        setTimeout(() => {
+            light.change(new Yellow(light))
+        }, 2000)
     }
 }
 
@@ -41,15 +75,14 @@ class Yellow {
         this.light = light
     }
 
-    switch() {
-        light.change(new Red(light))
+    switch({ light }) {
+        setTimeout(() => {
+            light.change(new Red(light))
+        }, 1000)
     }
 }
 
-
-function run() {
-    var light = new TrafficLight();
+const run = (function() {
+    let light = new TrafficLight();
     light.start();
- 
-    //log.show();
-}
+})();
